cea2ae15ebb6398f3f90173a17c653eb28de137b
Ruwanee Rathnayaka <ruwanithakshila2003@gmail.com>
2026-01-03 00:15:53 +0530

    Added new models and features

---
+++ CancerCare.WebApp/Models/Appointment.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CancerCare.WebApp.Models
+{
+    public class Appointment
+    {
+        [DisplayName("FullName")]
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public int TelNo { get; set; }
+        [Required]
+        [DisplayName("Doctor Name")]
+        public string DoctorName { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime AppointmentDate { get; set; }
+        public string Status { get; set; } = "Scheduled";
+    }
+}
