e2340509b8ccfa7942cced2454f35420c285f504
PamodaAmarawansha <pamodafhs23@gmail.com>
2026-01-03 06:00:31 +0530

    Updated About Us and Info

---
+++ CancerCare.WebApp/Controllers/InfoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CancerCare.WebApp.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
