49bb4ccd7b8cdb7c370492f10abfacd44c8d1b78
PamodaAmarawansha <pamodafhs23@gmail.com>
2026-01-03 06:32:29 +0530

    Updated Home and Appointment

---
+++ CancerCare.WebApp/Controllers/SignUpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CancerCare.WebApp.Controllers
+{
+    public class SignUpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
